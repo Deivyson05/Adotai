f0ac1cefae85a55f0626c7482445c5a87e01a0f0
DeivRicardoSS <deivysonrssantos@outlook.com>
2025-05-09 18:49:39 -0300

    finish page

--- front-end/src/pages/register/Finish/index.tsx
+++ front-end/src/pages/register/Finish/index.tsx
@@ -1,5 +1,21 @@
+import styles from "./styles.module.css";
+import { CheckCircle } from "@phosphor-icons/react";
+import { useNavigate } from "react-router-dom";
+
 export function Finish() {
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        navigate('/match');
+    }, 5000);
+
     return (
-        <main>Finish</main>
+        <main className={styles.container}>
+            <CheckCircle className={styles.icon} size={100} weight="fill" />
+            <div>
+                <strong>Cadastro realizado com sucesso!</strong>
+                <span>Redirecionando para a página inicial.</span>
+            </div>
+        </main>
     )
 }
