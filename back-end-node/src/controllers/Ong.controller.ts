0a847d283c2b7a56bb2dae919c9545a7746740e8
Brenno Souza | NcX <brenno.commercial@gmail.com>
2025-05-09 21:38:42 -0300

    FEATURE: Ong Controller³

--- back-end-node/src/controllers/Ong.controller.ts
+++ back-end-node/src/controllers/Ong.controller.ts
@@ -4,26 +4,26 @@ import createToken from "../core/token.ts";
 import { comparePassword, hashPassword } from "../core/hash.ts";
 
 class OngController {
-    async function createOng(req: Request, res: Response) {
-    const {
-        about,
-        address,
-        organization,
-        login
-    } = req.body;
+    async createOng(req: Request, res: Response) {
+        const {
+            about,
+            address,
+            organization,
+            login
+        } = req.body;
 
-    if (
-        !about ||
-        !address ||
-        !organization ||
-        !login
-    ) {
-        return res.sendStatus(400);
-    }
+        if (
+            !about ||
+            !address ||
+            !organization ||
+            !login
+        ) {
+            return res.sendStatus(400);
+        }
 
-    try {
-        const client = await db();
-        await client.query(`
+        try {
+            const client = await db();
+            await client.query(`
                     INSERT INTO Usuarios (
                         user_id,
                         tipo_id,
@@ -37,17 +37,17 @@ class OngController {
                         $1, $2, $3, $4, $5, $6, $7, $8
                     )
                     `, [
-            about.cpf,
-            1,
-            about.name,
-            about.phone,
-            login.email,
-            await hashPassword(login.password),
-            createToken(),
-            'https://imgs.search.brave.com/awksT_Zoh8G9Qn5d-CbZP4gAPcl0EDxLP0J88fgAnB4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTg3/ODA1MTU2L3ZlY3Rv/ci9wcm9maWxlLXBp/Y3R1cmUtdmVjdG9y/LWlsbHVzdHJhdGlv/bi5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9Z2t2TERDZ3NI/SC04SGVRZTdKc2po/bE9ZNnZSQkprX3NL/VzlseWFMZ21Mbz0'
-        ]);
+                about.cpf,
+                1,
+                about.name,
+                about.phone,
+                login.email,
+                await hashPassword(login.password),
+                createToken(),
+                'https://imgs.search.brave.com/awksT_Zoh8G9Qn5d-CbZP4gAPcl0EDxLP0J88fgAnB4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTg3/ODA1MTU2L3ZlY3Rv/ci9wcm9maWxlLXBp/Y3R1cmUtdmVjdG9y/LWlsbHVzdHJhdGlv/bi5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9Z2t2TERDZ3NI/SC04SGVRZTdKc2po/bE9ZNnZSQkprX3NL/VzlseWFMZ21Mbz0'
+            ]);
 
-        await client.query(`
+            await client.query(`
                         INSERT INTO Ong (
                             user_id,
                             ong_nome,
@@ -60,13 +60,13 @@ class OngController {
                         )
                         
                     `, [
-            about.cpf,
-            organization.name,
-            organization.cpfOrCnpj,
-            organization.description,
-        ]);
+                about.cpf,
+                organization.name,
+                organization.cpfOrCnpj,
+                organization.description,
+            ]);
 
-        await client.query(`
+            await client.query(`
                 INSERT INTO Endereco (
                     user_id,
                     end_cep,
@@ -80,22 +80,35 @@ class OngController {
                     $1, $2, $3, $4, $5, $6, $7, $8
                 )
             `, [
-            about.cpf,
-            address.cep,
-            address.state,
-            address.city,
-            address.neighborhood,
-            address.street,
-            address.number,
-            address.complement
-        ]);
+                about.cpf,
+                address.cep,
+                address.state,
+                address.city,
+                address.neighborhood,
+                address.street,
+                address.number,
+                address.complement
+            ]);
 
-    } catch (error) {
-        console.error(error)
-        return res.sendStatus(400);
-    }
+        } catch (error) {
+            console.error(error)
+            return res.sendStatus(400);
+        }
         return res.sendStatus(200);
-}
+    }
+
+    async createPet(req: Request, res: Response) {
+        try {
+            const OngId = await client.query(`
+                SELECT ong_id FROM ong WHERE user_id = $1
+            `, [about.cpf]);
+        }
+        catch (error) {
+            console.error(error)
+            return res.sendStatus(400)
+        }
+        return res.sendStatus(400)
+    }
 }
 
 export default new OngController();
